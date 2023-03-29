299d790336cc74a56fe74b0468266b6223c8926e
jean232 <ancho057@uottawa.ca>
2023-03-29 09:39:56 -0400

    ..

--- siteweb/src/Home.js
+++ siteweb/src/Home.js
@@ -59,19 +59,19 @@ function App() {
           <div class="filter2">
             <h6>Capacité</h6>
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+              <input class="form-check-input" type="checkbox" value="simple" id="defaultCheck1"/>
               <label class="form-check-label" for="defaultCheck1">1 personne</label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="defaultCheck2"/>
+              <input class="form-check-input" type="checkbox" value="double" id="defaultCheck2"/>
               <label class="form-check-label" for="defaultCheck1">2 personnes</label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="defaultCheck3"/>
+              <input class="form-check-input" type="checkbox" value="triple" id="defaultCheck3"/>
               <label class="form-check-label" for="flexCheckChecked">3 personnes</label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="defaultCheck3"/>
+              <input class="form-check-input" type="checkbox" value="quadruple" id="defaultCheck3"/>
               <label class="form-check-label" for="flexCheckChecked">4 personnes</label>
             </div>
           </div>
@@ -89,34 +89,3 @@ function App() {
   );
 }
 export default App;
-
-/* {chambre.map(chambre => (
-            
-            <div class="card border-dark mb-3">
-
-              <div class="row g-0">
-                <div class="col-md-2">
-                  <h1> {chambre.nomhotel}</h1>
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title"><b></b>Description de la chambre</h5>
-                    <p class="card-text">Capacité de la chambre : lit {chambre.capacite}, 
-                      <br />
-                      La chambre posséde les commoditées suivantes: {chambre.commodite}
-                      <br />
-                      Cette chambre à aussi une {chambre.vuechambre}
-                      <br />
-                      Les problèmes concernant la chambre sont: {chambre.probleme}
-                      <br />
-                      Possibilité d'augmenter la capacité de la chambre: {chambre.etendue}
-                    </p>
-                    
-                  </div>
-                </div>
-                <div className="col"><h5>Prix: <b>{chambre.prix}$</b></h5></div>
-              </div>
-
-            </div>
-
-          ))} */
